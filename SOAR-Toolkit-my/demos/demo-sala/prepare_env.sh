b76bb292ea0f1038f94670ea9a328f35f94178af
geray_air <919179287@qq.com>
2026-03-08 21:25:23 +0800

    delete unuse

--- SOAR-Toolkit-my/demos/demo-sala/prepare_env.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "[prepare_env] start $(date '+%F %T')"
-
-uv pip install --no-deps -e ./sglang/python
-
-export SGLANG_SERVER_ARGS="${SGLANG_SERVER_ARGS:-} --log-level info"
-
-echo "[prepare_env] done"
